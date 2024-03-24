bc349babdff90a75fad817313ac8f8a467e95490
Dogie-C <caoyuqi1996@hotmail.com>
2024-03-24 06:42:40 +0100

    modify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scripts import chiikawa
 
 if __name__ == '__main__':
-    a = chiikawa.Chiikawa()
+    a = chiikawa.Chiikawa('caoyuqi1996@gmail.com', 'woshi6B19960613')
     a.run()
--- scripts/chiikawa.py
+++ scripts/chiikawa.py
@@ -3,9 +3,12 @@ from utilities import browser
 from sites.chiikawa.pages.login import Login
 
 class Chiikawa():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
     def run(self):
         driver = browser.get_chrome_driver()
         login = Login(driver)
 
-        login.login('caoyuqi1996@gmail.com', 'woshi6B19960613')
+        login.login(self.username, self.password)
