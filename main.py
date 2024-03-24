177eeaafdb45ecd84ef0c3265e32dec3fa5655da
Dogie-C <caoyuqi1996@hotmail.com>
2024-03-24 06:49:47 +0100

    update

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from scripts import chiikawa
 
 if __name__ == '__main__':
-    a = chiikawa.Chiikawa('caoyuqi1996@gmail.com', 'woshi6B19960613')
+    # 抢购信息
+    username = 'caoyuqi1996@gmail.com'
+    password =  'woshi6B19960613'
+    a = chiikawa.Chiikawa(username, password)
     a.run()
